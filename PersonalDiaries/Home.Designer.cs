9f025675b59b6ee58de08d4b9fce070d28e9a0b6
OmarAmgad220 <omaramgadaboseif@gmail.com>
2025-04-17 00:18:52 +0200

    Frist commit

--- PersonalDiaries/Home.Designer.cs
+++ PersonalDiaries/Home.Designer.cs
@@ -1,4 +1,7 @@
-﻿namespace PersonalDiaries
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace Personal_Diary_Application
 {
     partial class Home
     {
@@ -28,12 +31,73 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dark_mode_btn = new System.Windows.Forms.CheckBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.title = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tag = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.CreatedAt = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dark_mode_btn
+            // 
+            this.dark_mode_btn.AutoSize = true;
+            this.dark_mode_btn.Location = new System.Drawing.Point(596, 25);
+            this.dark_mode_btn.Name = "dark_mode_btn";
+            this.dark_mode_btn.Size = new System.Drawing.Size(77, 17);
+            this.dark_mode_btn.TabIndex = 0;
+            this.dark_mode_btn.Text = "Dark Mode";
+            this.dark_mode_btn.UseVisualStyleBackColor = true;
+            this.dark_mode_btn.CheckedChanged += new System.EventHandler(this.dark_mode_btn_CheckedChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.title,
+            this.tag,
+            this.CreatedAt});
+            this.dataGridView1.Location = new System.Drawing.Point(36, 123);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(343, 255);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // title
+            // 
+            this.title.HeaderText = "Title";
+            this.title.Name = "title";
+            // 
+            // tag
+            // 
+            this.tag.HeaderText = "Tag";
+            this.tag.Name = "tag";
+            // 
+            // CreatedAt
+            // 
+            this.CreatedAt.HeaderText = "CreatedAt";
+            this.CreatedAt.Name = "CreatedAt";
+            // 
+            // Home
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(686, 390);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.dark_mode_btn);
+            this.Name = "Home";
             this.Text = "Home";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private CheckBox dark_mode_btn;
+        private DataGridView dataGridView1;
+        private DataGridViewTextBoxColumn title;
+        private DataGridViewTextBoxColumn tag;
+        private DataGridViewTextBoxColumn CreatedAt;
     }
 }
--- PersonalDiaries/Home.cs
+++ PersonalDiaries/Home.cs
@@ -7,14 +7,49 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Oracle.DataAccess.Client;
+using Oracle.DataAccess.Types;
+using PersonalDiaries;
 
-namespace PersonalDiaries
+namespace Personal_Diary_Application
 {
-    public partial class Home: Form
+    public partial class Home : Form
     {
+        string ordb = "Data source=orcl;User Id=scott; Password=tiger;";
+        OracleConnection conn;
+
+        public Login user;
+
         public Home()
         {
             InitializeComponent();
+        }
+
+        private void dark_mode_btn_CheckedChanged(object sender, EventArgs e)
+        {
+            //conn = new OracleConnection(ordb);
+            //conn.Open();
+
+            //OracleCommand cmd = new OracleCommand();
+            //cmd.Connection = conn;
+            //cmd.CommandType = CommandType.Text;
+
+            if (dark_mode_btn.Checked)
+            {
+                //cmd.CommandText = "update Users set darkMode = 1 where username =:userName";
+                this.BackColor = Color.FromArgb(34, 36, 49);
+                this.ForeColor = Color.White;
+            }
+            else
+            {
+                //cmd.CommandText = "update Users set darkMode = 0 where username =:userName";
+                this.BackColor = Color.White;
+                this.ForeColor = Color.Black;
+            }
+
+            //cmd.Parameters.Add("userName", Login.username);
+            //cmd.ExecuteNonQuery();
+            //conn.Dispose();
         }
     }
 }

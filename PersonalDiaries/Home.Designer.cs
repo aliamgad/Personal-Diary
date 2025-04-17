d705f0675a184f76896bd44300165bdf29cfe8ca
OmarAmgad220 <omaramgadaboseif@gmail.com>
2025-04-17 03:32:38 +0200

    All Home function has ended

--- PersonalDiaries/Home.Designer.cs
+++ PersonalDiaries/Home.Designer.cs
@@ -33,18 +33,18 @@ namespace Personal_Diary_Application
         {
             this.dark_mode_btn = new System.Windows.Forms.CheckBox();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.title = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.tag = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.CreatedAt = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.New_Diary = new System.Windows.Forms.Button();
+            this.manageTags = new System.Windows.Forms.Button();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // dark_mode_btn
             // 
-            this.dark_mode_btn.AutoSize = true;
-            this.dark_mode_btn.Location = new System.Drawing.Point(596, 25);
+            this.dark_mode_btn.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dark_mode_btn.Location = new System.Drawing.Point(506, 25);
             this.dark_mode_btn.Name = "dark_mode_btn";
-            this.dark_mode_btn.Size = new System.Drawing.Size(77, 17);
+            this.dark_mode_btn.Size = new System.Drawing.Size(135, 35);
             this.dark_mode_btn.TabIndex = 0;
             this.dark_mode_btn.Text = "Dark Mode";
             this.dark_mode_btn.UseVisualStyleBackColor = true;
@@ -53,44 +53,59 @@ namespace Personal_Diary_Application
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.title,
-            this.tag,
-            this.CreatedAt});
-            this.dataGridView1.Location = new System.Drawing.Point(36, 123);
+            this.dataGridView1.Location = new System.Drawing.Point(12, 25);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(344, 255);
+            this.dataGridView1.Size = new System.Drawing.Size(437, 353);
             this.dataGridView1.TabIndex = 1;
-            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            this.dataGridView1.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellDoubleClick);
             // 
-            // title
+            // New_Diary
             // 
-            this.title.HeaderText = "Title";
-            this.title.Name = "title";
+            this.New_Diary.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.New_Diary.Location = new System.Drawing.Point(506, 94);
+            this.New_Diary.Name = "New_Diary";
+            this.New_Diary.Size = new System.Drawing.Size(135, 47);
+            this.New_Diary.TabIndex = 2;
+            this.New_Diary.Text = "New Diary";
+            this.New_Diary.UseVisualStyleBackColor = true;
+            this.New_Diary.Click += new System.EventHandler(this.New_Diary_Click);
             // 
-            // tag
+            // manageTags
             // 
-            this.tag.HeaderText = "Tag";
-            this.tag.Name = "tag";
+            this.manageTags.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.manageTags.Location = new System.Drawing.Point(455, 193);
+            this.manageTags.Name = "manageTags";
+            this.manageTags.Size = new System.Drawing.Size(100, 49);
+            this.manageTags.TabIndex = 4;
+            this.manageTags.Text = "Manage Tags";
+            this.manageTags.UseVisualStyleBackColor = true;
+            this.manageTags.Click += new System.EventHandler(this.manageTags_Click);
             // 
-            // CreatedAt
+            // comboBox2
             // 
-            this.CreatedAt.HeaderText = "CreatedAt";
-            this.CreatedAt.Name = "CreatedAt";
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(574, 210);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(100, 21);
+            this.comboBox2.TabIndex = 6;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
             // 
             // Home
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(686, 390);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.manageTags);
+            this.Controls.Add(this.New_Diary);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.dark_mode_btn);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
             this.Name = "Home";
             this.Text = "Home";
             this.Load += new System.EventHandler(this.Home_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -98,8 +113,8 @@ namespace Personal_Diary_Application
 
         private CheckBox dark_mode_btn;
         private DataGridView dataGridView1;
-        private DataGridViewTextBoxColumn title;
-        private DataGridViewTextBoxColumn tag;
-        private DataGridViewTextBoxColumn CreatedAt;
+        private Button New_Diary;
+        private Button manageTags;
+        private ComboBox comboBox2;
     }
 }
--- PersonalDiaries/Home.cs
+++ PersonalDiaries/Home.cs
@@ -19,7 +19,9 @@ namespace Personal_Diary_Application
         OracleConnection conn;
 
         public Login user;
-
+        public static bool isNew;
+        public static int userId;
+        public static int diaryId; 
         public Home()
         {
             InitializeComponent();
@@ -38,13 +40,13 @@ namespace Personal_Diary_Application
             {
                 cmd.CommandText = "update Users set darkmode = 1 where username =:userName";
                 this.BackColor = Color.FromArgb(34, 36, 49);
-                this.ForeColor = Color.White;
+                dark_mode_btn.ForeColor = Color.White;
             }
             else
             {
                 cmd.CommandText = "update Users set darkMode = 0 where username =:userName";
                 this.BackColor = Color.White;
-                this.ForeColor = Color.Black;
+                dark_mode_btn.ForeColor = Color.Black;
             }
 
             cmd.Parameters.Add("userName", Login.username);
@@ -52,13 +54,41 @@ namespace Personal_Diary_Application
             conn.Dispose();
         }
 
-        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
-        {
-
-        }
-
         private void Home_Load(object sender, EventArgs e)
         {
+            conn = new OracleConnection(ordb);
+            conn.Open();
+
+            OracleCommand cmd2 = new OracleCommand();
+            cmd2.Connection = conn;
+            cmd2.CommandType = CommandType.Text;
+            cmd2.CommandText = "select userid from  Users where username =:userName";
+            cmd2.Parameters.Add("userName", Login.username);
+            OracleDataReader dr2 = cmd2.ExecuteReader();
+            dr2.Read();
+            userId = Convert.ToInt32(dr2[0]);
+            dr2.Close();
+            
+
+
+            //////////////////////////////////////////////////////////////////////////////
+            string cmdstr = @"SELECT 
+                            d.diaryid ,d.title, t.tagName,d.createdAt
+                            FROM Diaries d
+                            LEFT JOIN Tags t ON d.tagId = t.tagId
+                            WHERE d.userId = :id";
+            OracleDataAdapter adapter = new OracleDataAdapter(cmdstr, ordb);
+            adapter.SelectCommand.Parameters.Add("id", userId);
+            DataSet ds = new DataSet();
+            adapter.Fill(ds);
+            dataGridView1.DataSource = ds.Tables[0];
+
+            conn.Dispose();
+
+
+
+
+            //////////////////////////////////////////////////////////////////////////////
             conn = new OracleConnection(ordb);
             conn.Open();
 
@@ -69,23 +99,103 @@ namespace Personal_Diary_Application
             cmd.Parameters.Add("userName", Login.username);
             OracleDataReader dr = cmd.ExecuteReader();
 
-
             dr.Read();
             
             if (dr[0].ToString() == "1")
             {
                 dark_mode_btn.Checked = true;
                 this.BackColor = Color.FromArgb(34, 36, 49);
-                this.ForeColor = Color.White;
+                dark_mode_btn.ForeColor = Color.White;
             }
             else
             {
                 this.BackColor = Color.White;
-                this.ForeColor = Color.Black;
+                dark_mode_btn.ForeColor = Color.Black;
             }
 
             dr.Close();
             conn.Dispose();
+
+
+
+
+            //////////////////////////////////////////////////////////////////////////////
+            conn = new OracleConnection(ordb);
+            conn.Open();
+
+            OracleCommand cmd3 = new OracleCommand();
+            cmd3.Connection = conn;
+            cmd3.CommandType = CommandType.Text;
+            cmd3.CommandText = "select tagname from  tags where userid =:id";
+            cmd3.Parameters.Add("id", userId);
+            OracleDataReader dr3 = cmd3.ExecuteReader();
+            comboBox2.Items.Add("All");
+
+            while (dr3.Read())
+            {
+                comboBox2.Items.Add(dr3[0]);
+            }
+
+            comboBox2.SelectedIndex = 0;
+
+            dr3.Close();
+            conn.Dispose();
+        }
+
+        private void New_Diary_Click(object sender, EventArgs e)
+        {
+            isNew = true;
+            Diary form = new Diary();
+            form.Show();
+            this.Hide();
+        }
+
+        private void manageTags_Click(object sender, EventArgs e)
+        {
+            Tags form = new Tags();
+            form.Show();
+            this.Hide();
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string cmdstr;
+            if (comboBox2.SelectedItem.ToString() == "All")
+            {
+                cmdstr = @"SELECT 
+                            d.diaryid,d.title, t.tagName,d.createdAt
+                            FROM Diaries d
+                            LEFT JOIN Tags t ON d.tagId = t.tagId
+                            WHERE d.userId = :id";
+            }
+            else
+            {
+                cmdstr = @"SELECT 
+                            d.diaryid,d.title, t.tagName,d.createdAt
+                            FROM Diaries d
+                            LEFT JOIN Tags t ON d.tagId = t.tagId
+                            WHERE d.userId = :id and t.tagname=:name";
+            }
+            
+            OracleDataAdapter adapter = new OracleDataAdapter(cmdstr, ordb);
+            adapter.SelectCommand.Parameters.Add("id", userId);
+            if(comboBox2.SelectedItem.ToString() != "All")
+            {
+                adapter.SelectCommand.Parameters.Add("name", comboBox2.SelectedItem.ToString());
+            }
+            DataSet ds = new DataSet();
+            adapter.Fill(ds);
+            dataGridView1.DataSource = ds.Tables[0];
+        }
+
+        private void dataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            diaryId = Convert.ToInt32(dataGridView1.CurrentRow.Cells[0].Value.ToString());
+            isNew = false;
+
+            Diary form = new Diary();
+            form.Show();
+            this.Hide();
         }
     }
 }
